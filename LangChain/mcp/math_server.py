88540b8905e24a931eabdd165e7505e0d7a3f39c
mjhwang <hmj0774@gmail.com>
2025-11-11 11:27:28 +0900

    no message

--- LangChain/mcp/math_server.py
+++ LangChain/mcp/math_server.py
@@ -1,5 +1,9 @@
 # math_server.py를 로컬 방식이기 때문에 클라이언트 측에서 호출하여 실행할 것임
+
 from mcp.server.fastmcp import FastMCP
+# mcp.server.fastmcp: 공식 MCP SDK(mcp 패키지)
+## host/port/path 없음 -> stdio 중심(가장 호환/안정)
+## 로컬/에이전트 붙일 때 사용
 
 mcp = FastMCP("Math")
 
--- LangChain/mcp/weather_server.py
+++ LangChain/mcp/weather_server.py
@@ -1,9 +1,6 @@
 # weather_server.py가 서버이기 때문에 먼저 실행시키고 client.py 실행
+
 from typing import List
-# from mcp.server.fastmcp import FastMCP
-# mcp.server.fastmcp: 공식 MCP SDK(mcp 패키지)
-## host/port/path 없음 -> stdio 중심(가장 호환/안정)
-## 로컬/에이전트 붙일 때 사용
 from fastmcp import FastMCP 
 # fastmcp: 별도 fastmcp 패키지
 ## host/port/path 제공
